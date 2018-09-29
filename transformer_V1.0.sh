0497639a9af35863dc811eff4750c9be8f9df98c
badubko <badubko@gmail.com>
2018-09-29 12:54:48 -0300

    Version inicial de transformer

---
+++ transformer_V1.0.sh
@@ -0,0 +1,37 @@
+# !/bin/bash
+#-------------------------------------------------------------------------------
+# Formatear datos
+#-------------------------------------------------------------------------------
+# Version:
+
+
+declare -a NOMBRE_COL
+
+# NOMBRE_COL[Indice]		COL			Nombre Excel
+
+NOMBRE_COL[0]="apellido"		#	A	Apellido
+NOMBRE_COL[1]="nombres"			#	B	Nombre	
+NOMBRE_COL[2]="estado"			#	C	Estado
+NOMBRE_COL[3]="comentarios"		#	D	Comentarios
+NOMBRE_COL[4]="F_ingreso"		#	E	Fecha de ingreso
+NOMBRE_COL[5]="socio"			#	F	Socio
+NOMBRE_COL[6]=""				# 	G	Proyectos	 N/A
+NOMBRE_COL[7]=""				#	H	Area de trabajo del proyecto
+NOMBRE_COL[8]=""				#	I	Aptitud
+NOMBRE_COL[9]="especialidad"	#	J	Especialidad (parsear y a otra tabla)
+NOMBRE_COL[10]=""				#	K	Asertividad
+NOMBRE_COL[11]=""				#	L	Sociabilidad
+NOMBRE_COL[12]=""				#	M	Actitud
+NOMBRE_COL[13]=""				#	N	Talleres
+NOMBRE_COL[14]=""				#	O	Actividades
+NOMBRE_COL[15]="acuerdo"		#	P	Acuerdo
+NOMBRE_COL[16]=""				#	Q	Z
+NOMBRE_COL[17]="telefono_1"		#	R	Telefono 1
+NOMBRE_COL[18]=""				#	S	Presentacion
+NOMBRE_COL[19]="referido_por"	#	T	Referido por
+NOMBRE_COL[20]="telefono_2"		#	U	Telefono 2
+NOMBRE_COL[21]="cuil"			#	V	CUIL	
+NOMBRE_COL[22]="e_mail"			#	W	E-mail
+NOMBRE_COL[23]="entrevistado_por" # X	Entrevistado por
+NOMBRE_COL[24]=""				#	Y	Item type
+NOMBRE_COL[25]=""				#	Z	PATH
