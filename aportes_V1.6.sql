6931b719f5b9d3baeb5c88e7fece5c81ec56b10b
badubko <badubko@gmail.com>
2018-11-01 19:48:48 -0300

    Nueva version de tratar las especialidades en t_users2

--- aportes_V1.6.sql
+++ aportes_V1.6.sql
@@ -2,9 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-DROP SCHEMA IF EXISTS aportesV1.6;
-CREATE SCHEMA aportesV1.6;
-USE aportesV1.6;
+DROP SCHEMA IF EXISTS aportesV16;
+CREATE SCHEMA aportesV16;
+USE aportesV16;
 
 
 --
@@ -29,28 +29,32 @@ CREATE TABLE t_users1 (
 CREATE TABLE t_users2 (
   dni INT UNSIGNED NOT NULL,
   cuil VARCHAR(14) DEFAULT NULL,
+  especialidad SET ('Administración','Coaching','Comercial','Comunicacion',
+  'Contable','Diagnostico','Estrategia',
+  'Indicadores','Logistica','Negocios','Procesos',
+  'RRHH','Sistemas','No Informada') DEFAULT 'No Informada',
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   KEY idx_fk_dni (dni),
   CONSTRAINT fk_users2_dni FOREIGN KEY (dni) REFERENCES t_users1 (dni) ON DELETE RESTRICT ON UPDATE CASCADE
   )ENGINE=InnoDB DEFAULT CHARSET=utf8;
  
   
-CREATE TABLE t_especialidad_user (
-  dni INT UNSIGNED NOT NULL,
-  especialidad VARCHAR(14) DEFAULT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  KEY idx_fk_dni (dni),
-  CONSTRAINT fk_especialidad_dni FOREIGN KEY (dni) REFERENCES t_users1 (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
-  KEY idx_fk_especialidad (especialidad),
-  CONSTRAINT fk_especialidades_especialidad FOREIGN KEY (especialidad) REFERENCES t_especialidades (especialidad) ON DELETE RESTRICT ON UPDATE RESTRICT
-  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- CREATE TABLE t_especialidad_user (
+  -- dni INT UNSIGNED NOT NULL,
+  -- especialidad VARCHAR(14) DEFAULT NULL,
+  -- last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  -- KEY idx_fk_dni (dni),
+  -- CONSTRAINT fk_especialidad_dni FOREIGN KEY (dni) REFERENCES t_users1 (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+  -- KEY idx_fk_especialidad (especialidad),
+  -- CONSTRAINT fk_especialidades_especialidad FOREIGN KEY (especialidad) REFERENCES t_especialidades (especialidad) ON DELETE RESTRICT ON UPDATE RESTRICT
+  -- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
   
 CREATE TABLE t_estado_user (
   dni INT UNSIGNED NOT NULL,
   estado ENUM ('Asignado','Disponible','ND_Temp','De_Baja','Con_Restricc','Interno','Puntual','Desconoc') DEFAULT 'Desconoc',
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  consideraciones TINYTEXT (256) DEFAULT NULL,   
+  consideraciones VARCHAR (256) DEFAULT NULL,   
   KEY idx_fk_dni (dni),
   CONSTRAINT fk_estado_dni FOREIGN KEY (dni) REFERENCES t_users1 (dni) ON DELETE RESTRICT ON UPDATE CASCADE
   )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -58,15 +62,15 @@ CREATE TABLE t_estado_user (
 -- Tablas fijas
 
 CREATE TABLE t_roles (
-	rol VARCHAR 6 NOT NULL,
-	descr_rol VARCHAR 32 DEFAULT NULL,
+	rol VARCHAR (6) NOT NULL,
+	descr_rol VARCHAR (40) DEFAULT NULL,
 	PRIMARY KEY (rol)
 	)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE t_especialidades (
   especialidad VARCHAR(14) NOT NULL,
   PRIMARY KEY  (especialidad)
-  )
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
   
 -- CREATE TABLE t_estados (
   -- estado ENUM ('Asignado','Disponible','ND_Temp','De_Baja','Con_Restricc','Interno','Puntual','Desconoc') DEFAULT 'Desconoc',
@@ -81,7 +85,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- Carga de datos en Tablas Fijas
 
-\. SQL_Ins_T_fijas/2018-10-31_0018_ESPEC.sql
+-- \. SQL_Ins_T_fijas/2018-10-31_0018_ESPEC.sql
 
 INSERT INTO t_roles (rol,descr_rol)
 VALUES
